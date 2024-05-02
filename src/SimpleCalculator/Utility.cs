5f7fb0626018e97c2915d72ae4ece9b0209bdb31
Guilherme Montosa <guilherme.montosa@wexinc.com>
2024-05-02 17:39:36 -0300

    adding utility class logic

--- src/SimpleCalculator/Utility.cs
+++ src/SimpleCalculator/Utility.cs
@@ -10,8 +10,52 @@ namespace SimpleCalculator
     {
         public double EvaluateCalculation(string text)
         {
+            string numberLeft = "";
+            string numberRight = "";
 
+            for(var i = 0; i < text.Length; i++)
+            {
+                if (int.TryParse(text[i].ToString(), out int _))
+                {
+                    numberLeft = numberLeft + text[i];
+                }
+
+                if (IsAnLowerOperation(text[i]))
+                {
+                    // handle it
+                }
+
+                if (IsAnHigherOperation(text[i]))
+                {
+                    // handle it
+                }
+            }
+            
             throw new NotImplementedException();
+        }
+
+        public static bool IsAnLowerOperation(char text)
+        {
+            switch(text)
+            {
+                case '+':
+                case '-':
+                    return true;
+                default:
+                    return false;
+            }
+        }
+
+        public static bool IsAnHigherOperation(char text)
+        {
+            switch (text)
+            {
+                case '*':
+                case '/':
+                    return true;
+                default:
+                    return false;
+            }
         }
     }
 }
